acf4bb4493f57c6f5f37478e590318fe490e06af
harshawije97 <harshawije99@gmail.com>
2026-08-12 23:08:41 +0530

    api client setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,6 +5,7 @@ dotenv.config();
 
 const app = express();
 
+// Public routes
 app.get("/", (req, res) => {
   res.send("Hello from the server!");
 });
@@ -12,6 +13,12 @@ app.get("/", (req, res) => {
 app.get("/not-found", (_req, res) => {
   res.status(404).send("Not found");
 });
+
+app.post("/register", (_req, res) => {
+  res.send("Register");
+});
+
+// Auth routes - Protected routes
 
 const PORT = process.env.PORT || 3000;
 
