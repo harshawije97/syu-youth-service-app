029db22d8405459b9db240eb1d7feede1fc29aac
harshawije97 <harshawije99@gmail.com>
2026-08-16 15:35:03 +0530

    update

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,6 +8,7 @@ import {
 import cors from "cors";
 import { dbClient } from "./database/client.js";
 import {
+  getAllAttendances,
   getAllRegistrations,
   getRegistrationById,
   registrationsByQR,
@@ -71,7 +72,7 @@ app.post("/attendance", async (req, res) => {
 
 app.get("/attendance/all", async (req: Request, res: Response) => {
   try {
-    const response = await getAllRegistrations();
+    const response = await getAllAttendances();
 
     res.status(200).json({
       success: true,
--- server/src/queries/mongodb-query.ts
+++ server/src/queries/mongodb-query.ts
@@ -15,16 +15,23 @@ export const registrationsByQR = async (data: QrRecord) => {
 
 // Get all registrations
 export const getAllRegistrations = async () => {
-  const registrations = await Attendance.find({});
+  // Registrations by qr
+  const registrations = await Registration.find({});
 
   return registrations;
 };
 
 // Get registrations by Id
 export const getRegistrationById = async (id: string) => {
-  const registration = await Attendance.find({
+  const registration = await Registration.find({
     id: id,
   });
 
   return registration;
+};
+
+export const getAllAttendances = async () => {
+  const attendances = await Attendance.find({});
+
+  return attendances;
 };
